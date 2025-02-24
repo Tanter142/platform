ce00fd322448e3f17d6449b8f269878e8944b86d
Tanter142 <edikromanchenko7@gmail.com>
2025-02-24 17:22:33 +0300

    перевод

--- frontend/src/pages/Courses.vue
+++ frontend/src/pages/Courses.vue
@@ -4,8 +4,8 @@
 			class="sticky top-0 z-10 flex items-center justify-between border-b bg-white px-3 py-2.5 sm:px-5"
 		>
 			<Breadcrumbs
-				class="h-10"
-				:items="[{ label: __('Courses'), route: { name: 'Courses' } }]"
+				class="h-7"
+				:items="[{ label: __('Courses'), route: { name: 'Курсы' } }]"
 			/>
 			<div class="flex space-x-2 justify-end">
 				<div class="w-40 md:w-44">
