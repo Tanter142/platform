34742c6dab5c1072bce9c9bbc2043a5d39be957f
Tanter142 <edikromanchenko7@gmail.com>
2025-03-01 17:55:47 +0300

    Перевод модального окна редактирования профиля

--- frontend/src/components/Modals/EditProfile.vue
+++ frontend/src/components/Modals/EditProfile.vue
@@ -131,7 +131,7 @@ const imageResource = createResource({
 const updateProfile = createResource({
 	url: 'frappe.client.set_value',
 	makeParams(values) {
-		profile.bio = escapeHTML(profile.bio)
+		profile.bio = profile.bio
 		return {
 			doctype: 'User',
 			name: props.profile.data.name,
