d64a9907fb1ae9cd40ed2fb3758acb8d381171e3
Tanter142 <edikromanchenko7@gmail.com>
2025-03-01 16:10:50 +0300

    Перевод

--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -152,7 +152,7 @@ const setActiveTab = () => {
 			activeTab.value = convertToTitleCase(section)
 		}
 	})
-	if (!activeTab.value) activeTab.value = 'About'
+	if (!activeTab.value) activeTab.value = 'Aboba'
 }
 
 watchEffect(() => {
