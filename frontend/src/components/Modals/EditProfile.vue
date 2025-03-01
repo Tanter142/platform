aea4894b52b2a0a70f6557cadd534d709bc62503
Tanter142 <edikromanchenko7@gmail.com>
2025-03-01 17:55:01 +0300

    Перевод модального окна редактирования профиля

--- frontend/src/components/Modals/EditProfile.vue
+++ frontend/src/components/Modals/EditProfile.vue
@@ -75,8 +75,9 @@
 						{{ __('Описание') }}
 					</div>
 					<TextEditor
-						v-model="profile.bio"
 						:fixedMenu="true"
+						@change="(val) => (profile.bio = val)"
+						:content="profile.bio"
 						editorClass="prose-sm py-2 px-2 min-h-[200px] border-gray-300 hover:border-gray-400 rounded-md bg-gray-200"
 					/>
 				</div>
