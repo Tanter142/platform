21311f9bdef6ff750ec6c7b1720338c8abe20f83
Tanter142 <edikromanchenko7@gmail.com>
2025-03-18 01:16:15 +0300

    применение стилей

--- frontend/src/pages/LessonForm.vue
+++ frontend/src/pages/LessonForm.vue
@@ -454,7 +454,7 @@ const showToast = (title, text, icon) => {
 				: 'bg-red-600 text-white rounded-md p-1 flex items-center justify-center',
 		containerClass: 'flex items-center gap-2 p-3 bg-white shadow-md rounded-lg',
 		position: icon == 'check' ? 'bottom-right' : 'top-center',
-		timeout: icon == 0,
+		timeout: icon == 'check' ? 5 : 10,
 	})
 }
 
