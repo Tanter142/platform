e2fc2c9a9d1ef6e9c5c911eec25a3fdaac049faf
Tanter142 <edikromanchenko7@gmail.com>
2025-03-17 22:59:05 +0300

    исправление добавления пустого тега <p> в профиле

--- frontend/src/components/Modals/EditProfile.vue
+++ frontend/src/components/Modals/EditProfile.vue
@@ -76,7 +76,7 @@
 					</div>
 					<TextEditor
 						:fixedMenu="true"
-						@change="(val) => (profile.bio = val)"
+						@change="(val) => profile.bio = val.trim() === '<p></p>' ? '' : val"
 						:content="profile.bio"
 						editorClass="prose-sm py-2 px-2 min-h-[200px] border-gray-300 hover:border-gray-400 rounded-md bg-gray-200"
 					/>
