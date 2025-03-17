3b9253f8a3580c8c37a6d26dd920b1c94d8eb098
Tanter142 <edikromanchenko7@gmail.com>
2025-03-18 01:48:20 +0300

    Исправление стилей

--- frontend/src/components/Modals/ChapterModal.vue
+++ frontend/src/components/Modals/ChapterModal.vue
@@ -78,7 +78,7 @@ import {
 	Switch,
 } from 'frappe-ui'
 import { defineModel, reactive, watch } from 'vue'
-import { showToast, getFileSize } from '@/utils/'
+import { getFileSize, createToast } from '@/utils/'
 import { capture } from '@/telemetry'
 import { FileText, X } from 'lucide-vue-next'
 import { useSettings } from '@/stores/settings'
@@ -96,6 +96,23 @@ const props = defineProps({
 		type: Object,
 	},
 })
+
+const showToast = (title, text, icon) => {
+	createToast({
+		title: title,
+		text: `<span class="${
+			icon == 'check' ? 'leading-[1.4]' : 'leading-[1.4]'
+		}">${text}</span>`,
+		icon: icon,
+		iconClasses:
+			icon == 'check'
+				? 'bg-green-600 text-white rounded-md p-1 flex items-center justify-center'
+				: 'bg-red-600 text-white rounded-md p-1 flex items-center justify-center',
+		containerClass: 'flex items-center gap-2 p-3 bg-white shadow-md rounded-lg',
+		position: icon == 'check' ? 'bottom-right' : 'top-center',
+		timeout: icon == 'check' ? 5 : 10,
+	})
+}
 
 const chapter = reactive({
 	title: '',
