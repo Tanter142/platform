88417e4cbfd27c408f1d935ef310b7715e693086
Tanter142 <edikromanchenko7@gmail.com>
2025-03-18 00:02:48 +0300

    Исрпавление невозможнорсти удалить курс

--- lms/lms/api.py
+++ lms/lms/api.py
@@ -574,7 +574,7 @@ def update_source_chapter(lesson, chapter, idx, hasMoved=False):
 
 	lessons.remove(lesson)
 	if not hasMoved:
-		frappe.db.delete("Lesson Reference", {"parent": chapter, "lesson": lesson}, force=True)
+		frappe.db.delete("Lesson Reference", {"parent": chapter, "lesson": lesson}, )
 	else:
 		lessons.insert(idx, lesson)
 
@@ -884,7 +884,7 @@ def delete_course(course):
 		)
 
 		for lesson in lesson_references:
-			frappe.delete_doc("Lesson Reference", lesson, force=True)
+			frappe.delete_doc("Lesson Reference", lesson)
 
 		for lesson in lessons:
 			topics = frappe.get_all(
@@ -1066,10 +1066,10 @@ def delete_chapter(chapter):
 	if chapterInfo.is_scorm_package:
 		delete_scorm_package(chapterInfo.scorm_package_path)
 
-	frappe.db.delete("Chapter Reference", {"chapter": chapter}, force=True)
-	frappe.db.delete("Lesson Reference", {"parent": chapter}, force=True)
-	frappe.db.delete("Course Lesson", {"chapter": chapter}, force=True)
-	frappe.db.delete("Course Chapter", chapter, force=True)
+	frappe.db.delete("Chapter Reference", {"chapter": chapter}, )
+	frappe.db.delete("Lesson Reference", {"parent": chapter}, )
+	frappe.db.delete("Course Lesson", {"chapter": chapter}, )
+	frappe.db.delete("Course Chapter", chapter, )
 
 
 def delete_scorm_package(scorm_package_path):
