8d185bd4d93304406efa50252f19735710fdd262
Haunted <skorokhodov.lenya@gmail.com>
2025-04-10 20:31:10 +0300

    test

--- frontend/src/pages/Assignments.vue
+++ frontend/src/pages/Assignments.vue
@@ -1,6 +1,6 @@
 <template>
 	<header
-		class="sticky top-0 z-10 flex items-center justify-between border-b bg-white px-3 py-2.5 sm:px-5"
+		class="sticky top-0 z-10 flex items-center justify-between border-b bg-lavender-100 px-3 py-2.5 sm:px-5"
 	>
 		<Breadcrumbs :items="breadcrumbs" />
 		<router-link
