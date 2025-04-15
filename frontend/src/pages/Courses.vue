2d3dc25e1f6904c9c6aa0171d565dfa0bab52a6a
Haunted <skorokhodov.lenya@gmail.com>
2025-04-15 15:36:38 +0300

    12

--- frontend/src/pages/Courses.vue
+++ frontend/src/pages/Courses.vue
@@ -1,7 +1,7 @@
 <template>
 	<div v-if="courses.data">
 		<header
-			class="sticky top-0 z-10 flex items-center justify-between border-b bg-blue px-3 py-2.5 sm:px-5"
+			class="sticky top-0 z-10 flex items-center justify-between border-b bg-#5858f6 px-3 py-2.5 sm:px-5"
 		>
 			<Breadcrumbs
 				class="h-7"
