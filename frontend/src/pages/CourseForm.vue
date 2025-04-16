f179d9272f82824e33cad28492e693327cc0e77e
Haunted <skorokhodov.lenya@gmail.com>
2025-04-16 21:56:28 +0300

    Инпуты Форма Курсов

--- frontend/src/pages/CourseForm.vue
+++ frontend/src/pages/CourseForm.vue
@@ -593,6 +593,7 @@ updateDocumentTitle(pageMeta)
 	border-color: #5a5cff !important;
 	border-width: 1px !important;
 	border-radius: 10px !important;
+	background-color: #e1e5e8 !important;
 }
 
 </style>
