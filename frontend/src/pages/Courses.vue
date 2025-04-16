9a97e5aee09d63965ca8a58fde625f84443662a5
Haunted <skorokhodov.lenya@gmail.com>
2025-04-16 20:58:26 +0300

    Создание курса 7

--- frontend/src/pages/Courses.vue
+++ frontend/src/pages/Courses.vue
@@ -320,7 +320,7 @@ updateDocumentTitle(pageMeta)
 	background-color: #3f3fff !important;
 }
 .create-course{
-	background-color: rgba(128, 128, 128, 0.2) !important;
+	background-color: rgba(128, 128, 128, 0.4) !important;
 	color: white !important;
 	border-radius: 5px !important;
 	border: solid 1px rgba(114, 113, 113, 0.2) !important;
