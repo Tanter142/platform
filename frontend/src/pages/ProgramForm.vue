151050a9743899528fd84473fcd50af524875f7f
Haunted <skorokhodov.lenya@gmail.com>
2025-04-19 15:19:37 +0300

    тест

--- frontend/src/pages/ProgramForm.vue
+++ frontend/src/pages/ProgramForm.vue
@@ -3,7 +3,7 @@
 		class="sticky top-0 z-10 flex flex-col md:flex-row md:items-center justify-between border-b bg-white px-3 py-2.5 sm:px-5"
 	>
 		<Breadcrumbs :items="breadbrumbs" />
-		<Button variant="solid" @click="saveProgram()" сlass="save-button">
+		<Button variant="solid" @click="saveProgram()" class="save-button">
 			{{ __('Сохранить') }}
 		</Button>
 	</header>
